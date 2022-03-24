623288ec14e369b127fdba148b6251d6a8ce57ed
asafworld <gabrielasaf10@gmail.com>
2022-03-24 19:34:07 -0300

    Adiciona o requisito 9

--- helpers/fetchItem.js
+++ helpers/fetchItem.js
@@ -3,13 +3,12 @@ const fetchItem = async (itemID) => {
   const url = `https://api.mercadolibre.com/items/${itemID}`;
   const response = await fetch(url);
   const responseJson = await response.json();
-  console.log(responseJson);
   return responseJson;
  } catch (err) {
   return err;
  }
 };
-console.log(fetchItem('MLB1341706310'));
+
 if (typeof module !== 'undefined') {
   module.exports = {
     fetchItem,
--- tests/fetchItem.test.js
+++ tests/fetchItem.test.js
@@ -3,6 +3,32 @@ const { fetchItem } = require('../helpers/fetchItem');
 const item = require('../mocks/item');
 
 describe('2 - Teste a função fecthItem', () => {
-  // implemente seus testes aqui
-  fail('Teste vazio');
+  it('Testa de fetchItem é uma função.', async () => {
+    expect.assertions(1);
+    await expect(typeof fetchItem).toBe('function');
+  });
+
+  it('Testa se fetch é chamado ao passo que fetchItem é executada com parâmetro *MLB1615760527*', async () => {
+    expect.assertions(1);
+    await fetchItem('MLB1615760527');
+    expect(fetch).toHaveBeenCalledTimes(1);
+  });
+
+  it('Testa se ao executar fetchItem com parâmetro *MLB1615760527*, fetch é chamado com a url correta.', async () => {
+    expect.assertions(1);
+    await fetchItem('MLB1615760527');
+    expect(fetch).toHaveBeenCalledWith('https://api.mercadolibre.com/items/MLB1615760527');
+  });
+
+  it('Testa se o retorno da função fetchItem(MLB1615760527) é um objeto igual a item', async () => {
+    expect.assertions(1);
+    const response = await fetchItem('MLB1615760527');
+    expect(response).toEqual(item);
+  });
+
+  it('Testa se, ao executar fetchItem sem argumento, retorna mensagem de erro', async () => {
+    expect.assertions(1);
+    const response = await fetchItem();
+    expect(response).toEqual(new Error('You must provide an url'))
+  });
 });

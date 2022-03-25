6467dd9cbf30240baf1d9211c048c381197a5532
asafworld <gabrielasaf10@gmail.com>
2022-03-25 10:20:28 -0300

    Entrega o requisito 3

--- script.js
+++ script.js
@@ -39,8 +39,8 @@ function getSkuFromProductItem(item) {
   return item.querySelector('span.item__sku').innerText;
 }
 
-function cartItemClickListener() {
-  // coloque seu código aqui (talvez o parâmetro seja um event)
+function cartItemClickListener(event) {
+  event.target.remove();
 }
 
 function createCartItemElement({ sku, name, salePrice }) {
